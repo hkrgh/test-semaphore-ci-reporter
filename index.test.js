28088ac3bc550588289fb8282da3272fc1dd7176
hkrgh <40249649+hkrgh@users.noreply.github.com>
2018-06-14 18:30:06 -0700

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -2,4 +2,8 @@ describe('A test file', () => {
   test('this test should pass', () => {
     expect(true).toBe(true)
   })
+  
+  test('this test should again pass', () => {
+    expect(true).toBe(true)
+  })
 })
